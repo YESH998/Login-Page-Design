a1c233f8ee59981e7845c0879fd5d5b5cc831646
yesh <yeshchoudhari68@gmail.com>
2024-08-17 15:58:38 +0530

    Login Page

---
+++ script.js
@@ -0,0 +1,71 @@
+let leftElement1 = document.getElementById('login');
+
+leftElement1.addEventListener('click',function(){
+
+    let inputElement = document.getElementById('loginUserName');
+
+    let inputValue = inputElement.value;
+
+    console.log(inputValue);
+    
+});
+
+let leftElement2 = document.getElementById('login');
+
+leftElement2.addEventListener('click',function(){
+
+    let inputElement = document.getElementById('loginPassword');
+
+    let inputValue = inputElement.value;
+
+    console.log(inputValue);
+    
+});
+
+let rightElement1 = document.getElementById('myButton');
+
+rightElement1.addEventListener('click', function () {
+
+    let inputElement = document.getElementById('userName');
+
+    let inputValue = inputElement.value;
+
+    console.log(inputValue);
+
+});
+
+let rightElement2 = document.getElementById('myButton');
+
+rightElement2.addEventListener('click', function () {
+
+    let inputElement = document.getElementById('Password');
+
+    let inputValue = inputElement.value;
+
+    console.log(inputValue);
+
+});
+
+let rightElement3 = document.getElementById('myButton');
+
+rightElement3.addEventListener('click', function () {
+
+    let inputElement = document.getElementById('Email');
+
+    let inputValue = inputElement.value;
+
+    console.log(inputValue);
+
+});
+
+let rightElement4 = document.getElementById('myButton');
+
+rightElement4.addEventListener('click', function () {
+
+    let inputElement = document.getElementById('reTypePassword');
+
+    let inputValue = inputElement.value;
+
+    console.log(inputValue);
+
+});
